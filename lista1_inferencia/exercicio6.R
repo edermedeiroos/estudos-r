a511e36b18d6d09edd19248ede12f42a04f3431b
Madeira <ederlissemedeiros@gmail.com>
2026-03-24 16:48:39 -0300

    Exercício 6

--- lista1_inferencia/exercicio6.R
+++ lista1_inferencia/exercicio6.R
@@ -1,24 +1,54 @@
-lambda_dia = 3
-lambda_2dias = lambda_dia * 2
-lambda_12h = lambda_dia / 2
+# Item a
+x_bin <- 0:10
 
-acidentes_a = 2
-prob_a = dpois(acidentes_a, lambda_dia)
+y_bin1 <- dbinom(x_bin, size = 10, prob = 0.2)
+y_bin2 <- dbinom(x_bin, size = 10, prob = 0.5)
+y_bin3 <- dbinom(x_bin, size = 10, prob = 0.8)
 
-acidentes_b = 0
-prob_b = 1 - dpois(acidentes_b, lambda_dia)
+plot(x_bin, y_bin1, type = "b", col = "red", pch = 16, lwd = 2,
+     main = "Distribuições Binomiais (n=10)",
+     xlab = "Número de Sucessos (x)", ylab = "Probabilidade P(X=x)",
+     ylim = c(0, 0.35))
 
-acidentes_c = 4
-prob_c = ppois(acidentes_c, lambda_dia)
+lines(x_bin, y_bin2, type = "b", col = "blue", pch = 17, lwd = 2)
+lines(x_bin, y_bin3, type = "b", col = "green", pch = 18, lwd = 2)
 
-acidentes_d = 5
-prob_d = dpois(acidentes_d, lambda_2dias)
+legend("topright", legend = c("p = 0.2", "p = 0.5", "p = 0.8"),
+       col = c("red", "blue", "green"), pch = c(16, 17, 18), lty = 1, lwd = 2)
 
-acidentes_e = 0
-prob_e = dpois(acidentes_e, lambda_12h)
+# Item b
+x_norm <- seq(-6, 6, length = 1000)
 
-prob_a
-prob_b
-prob_c
-prob_d
-prob_e
+y_norm1 <- dnorm(x_norm, mean = 0, sd = 1)
+y_norm2 <- dnorm(x_norm, mean = 0, sd = 2)
+y_norm3 <- dnorm(x_norm, mean = 2, sd = 1)
+
+plot(x_norm, y_norm1, type = "l", col = "red", lwd = 2,
+     main = "Distribuições Normais",
+     xlab = "Valores de X", ylab = "Densidade",
+     ylim = c(0, 0.45))
+
+lines(x_norm, y_norm2, col = "blue", lwd = 2, lty = 2)
+lines(x_norm, y_norm3, col = "green", lwd = 2, lty = 3)
+
+legend("topright", legend = c("N(0,1)", "N(0,4)", "N(2,1)"),
+       col = c("red", "blue", "green"), lty = c(1, 2, 3), lwd = 2)
+
+# Item c
+x_pois <- 0:20
+
+y_pois1 <- dpois(x_pois, lambda = 2)
+y_pois2 <- dpois(x_pois, lambda = 5)
+y_pois3 <- dpois(x_pois, lambda = 10)
+
+plot(x_pois, y_pois1, type = "b", col = "red", pch = 16, lwd = 2,
+     main = "Distribuições de Poisson",
+     xlab = "Número de Ocorrências (x)", ylab = "Probabilidade P(X=x)",
+     ylim = c(0, 0.3))
+
+lines(x_pois, y_pois2, type = "b", col = "blue", pch = 17, lwd = 2)
+lines(x_pois, y_pois3, type = "b", col = "green", pch = 18, lwd = 2)
+
+legend("topright", legend = c("lambda = 2", "lambda = 5", "lambda = 10"),
+       col = c("red", "blue", "green"), pch = c(16, 17, 18), lty = 1, lwd = 2)
+
